ed7c4fd3901e4c8d3ceb20aa640d9bee647d35fe
dikokkali <dikokkaliaroglou@gmail.com>
2022-01-21 01:46:51 +0200

    Added basic camera movement

---
+++ Assets/Scripts/BuildingMode/OverheadCamera/OverheadCameraController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class OverheadCameraController : MonoBehaviour
+{
+    private CinemachineVirtualCamera _overheadCamera;
+
+    public float cameraMoveSpeed;
+    public float cameraMoveAcceleration;
+    public float cameraZoomSpeed;
+    public float cameraZoomAcceleration;
+
+
+    private void Awake()
+    {
+        _overheadCamera = GetComponent<CinemachineVirtualCamera>();   
+    }
+
+    private void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        float zoomDirection = Input.GetAxis("Mouse ScrollWheel");
+
+        PanCamera(moveX, moveY);
+        ZoomCamera(zoomDirection);
+    }
+
+    private void PanCamera(float x, float y)
+    {
+         Vector3 movement = new Vector3(x, 0f, y);
+
+        _overheadCamera.transform.position = Vector3.Lerp(_overheadCamera.transform.position,
+                                                          _overheadCamera.transform.position + movement * cameraMoveSpeed * Time.deltaTime,
+                                                             cameraMoveAcceleration);        
+    }
+
+    private void ZoomCamera(float zoom)
+    {
+        Vector3 zoomMovement = new Vector3(0f, zoom, 0f);
+
+        _overheadCamera.transform.position = Vector3.Lerp(_overheadCamera.transform.position,
+                                                          _overheadCamera.transform.position - zoomMovement * cameraZoomSpeed * Time.deltaTime,
+                                                          cameraZoomAcceleration);
+    }
+}
